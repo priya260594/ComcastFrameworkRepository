190bc7d7a4a067f6c1ea6209ef5e72774523f59c
User <User@DESKTOP-6P64TEG>
2024-10-01 11:03:49 +0530

    2 nd commit

--- src/main/java/com/VTiger/crm/ObjectRepositories/Org/dummyPage.java
+++ src/main/java/com/VTiger/crm/ObjectRepositories/Org/dummyPage.java
@@ -3,5 +3,6 @@ package com.VTiger.crm.ObjectRepositories.Org;
 public class dummyPage {
 public static void main(String[] args) {
 	System.out.println("i created this while cloing");
+	System.out.println("i created this while cloing");
 }
 }
